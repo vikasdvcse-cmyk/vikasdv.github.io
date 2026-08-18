28678f56283e1c1b6aafabf9523d6af3dd1befa3
Vikas D V <vikas.dv.cse@gmail.com>
2026-08-18 13:16:41 +0530

    Update this thing pulls random numbers from your rams memory.cpp

--- this thing pulls random numbers from your rams memory.cpp
+++ this thing pulls random numbers from your rams memory.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main() {
-    int a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z;
+    int a,b,c,d,e;
     std::cout<<a<<std::endl;
     std::cout<<b<<std::endl;
     std::cout<<c<<std::endl;
